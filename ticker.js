e1385699629f84e10162f8f1f126aba916eea325
Benjamin Adams <armastevs@gmail.com>
2017-12-22 14:57:30 -0600

    top ticker

---
+++ ticker.js
@@ -0,0 +1,45 @@
+setTimeout(function initGES () {
+    var $els = []    
+    var root=$('[class*="Navbar_nav-wrapper_"]')
+    var $ticker=$('<div class="topTicker"></div>')
+    root.append($ticker)
+    setInterval(updatePrices,1000)
+
+    function updatePrices(){
+        $.ajax({
+            method: "GET",
+            url: "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=20",
+            dataType: 'json'  
+          }).success(function(data,x,y){
+            if(!data || !data[0] ) return
+
+            data.forEach(function(pair) {
+                createOrUpdateSymbolEl(pair)  
+
+            });
+            
+
+        });
+    }
+
+    function createOrUpdateSymbolEl(pair){
+
+        var color='green'
+        if(parseFloat(pair.percent_change_1h) < 0 ){
+            color=red;
+        }
+
+        newHtml=pair.symbol + '<span class="'+color+' topPrice"> '+pair.price_usd+'</span>'
+
+
+        if($els[pair.symbol]) {
+             $els[pair.symbol].html(newHtml);
+        }else {
+            $els[pair.symbol] =$('<span class="topPair">'+pair.symbol+'</span>')
+            $ticker.append($els[pair.symbol])        
+            $els[pair.symbol].html(newHtml);
+        }
+    }
+
+
+}, 400)
