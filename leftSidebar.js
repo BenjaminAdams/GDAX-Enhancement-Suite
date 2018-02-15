17f06ca5d4bf7f3fc74347735112e53b6314c2e7
Benjamin Adams <armastevs@gmail.com>
2018-02-15 09:57:40 -0600

    leftsidebar has gdax/bitfinix/binance prices

--- leftSidebar.js
+++ leftSidebar.js
@@ -1,9 +1,55 @@
 window.startLeftSidebar = function () {
-    var $binanceBoxInner = $('<div class="binanceBox"></div>')
-    $('[class*="OrderForm_order-form_"]').after($binanceBoxInner)
-    var $binanceBox = $('.binanceBox')
+    var $binancePrice = 0
+    var $bitfinexPrice = 0
+    var $gdaxPrice = 0
 
-    setInterval(updateBinance, 600)
+    initRegions()
+    setInterval(updatePrices, 1000)
+    loadBitfinexWS()
+
+
+    function updatePrices() {
+        updateBinance()
+        updateGdax()
+
+    }
+
+    function updateGdax() {
+        if (window.currentPrice) {
+            $gdaxPrice.html(parseFloat(window.currentPrice).toFixed(2))
+        } else {
+            $gdaxPrice.html('-')
+        }
+
+    }
+
+    function loadBitfinexWS() {
+        var ws = new WebSocket('wss://api.bitfinex.com/ws');
+        ws.onopen = function () {
+            ws.send(JSON.stringify({ "event": "subscribe", "channel": "ticker", "pair": "BTCUSD" }));
+        };
+
+        ws.onmessage = function (msg) {
+            var data = JSON.parse(msg.data);
+            console.log(data)
+            var hb = data[1];
+            if (hb != "hb") {
+                var price = parseFloat(data[7]).toFixed(2)
+                if (price === NaN) return
+
+                if (price > window.currentPrice) {
+                    price = '<span class="green">' + price + '</span>'
+                } else {
+                    price = '<span class="red">' + price + '</span>'
+                }
+
+                $bitfinexPrice.html(price)
+            }
+        };
+    }
+
+
+
 
     function updateBinance() {
         $.ajax({
@@ -19,7 +65,25 @@ window.startLeftSidebar = function () {
             } else {
                 price = '<span class="red">' + price + '</span>'
             }
-            $binanceBox.html(price)
+            $binancePrice.html(price)
         });
+    }
+
+    function initRegions() {
+
+        var $bitfinexBoxInner = $('<div class="lsItem"><span class="lsItemTitle">Bitfinex</span> <span class="bitfinexPrice lsPrice">-</span></div>')
+        $('[class*="OrderForm_order-form_"]').after($bitfinexBoxInner)
+        $bitfinexPrice = $('.bitfinexPrice')
+
+
+        var $binanceBoxInner = $('<div class="lsItem"><span class="lsItemTitle">Binance</span> <span class="binancePrice lsPrice">-</span></div>')
+        $('[class*="OrderForm_order-form_"]').after($binanceBoxInner)
+        $binancePrice = $('.binancePrice')
+
+        var $gdaxBoxInner = $('<div class="lsItem"><span class="lsItemTitle">GDAX</span> <span class="gdaxPrice lsPrice">-</span></div>')
+        $('[class*="OrderForm_order-form_"]').after($gdaxBoxInner)
+        $gdaxPrice = $('.gdaxPrice')
+
+
     }
 }
