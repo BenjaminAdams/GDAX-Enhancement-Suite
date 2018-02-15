e11cea093d208c240b80bcbd435fbe6f1c7b4f44
Benjamin Adams <armastevs@gmail.com>
2018-02-14 18:03:01 -0600

    showing binance prices on page

--- content.js
+++ content.js
@@ -1,40 +1,63 @@
 
-window.GES= {
+window.GES = {
     currencySymbol: '$',
-    currencyIso:'USD'
-
+    currencyIso: 'USD'
 };
 
-setTimeout(function initGES () {
-    var currentPrice=0;
+window.processMyData = function processMyData(data) {
+    console.log(data);
+}
+
+
+setTimeout(function initGES() {
+    var currentPrice = 0;
     var $sellsOrderSizeInfo
     var $sellsOrderSizeTotal
     var $sellsSum
     var $buyOrderSizeTotal
     var $buysSum
-    var currencySymbol='$' //todo, find users local symbol
-    window.currencySymbol =currencySymbol
-    var currencyIso= 'USD'
+    var $binanceBox
+    var currencySymbol = '$' //todo, find users local symbol
+    window.currencySymbol = currencySymbol
+    var currencyIso = 'USD'
 
     addOrdersInfoBoxes()
 
-    setInterval(function(){
+    setInterval(function () {
         calculateOpenOrders('sell')
         calculateOpenOrders('buy')
-    },2000)
+    }, 2000)
 
-    setInterval(updateCurrentPrice,1000)
-    setInterval(checkOtherMarkets,1000)
+    setInterval(updateCurrentPrice, 1000)
+    setInterval(updateBinance, 600)
 
 
-    //https://blockchain.info/ticker only gets BTC
-    function checkOtherMarkets(){
-        //todo call some API to get the price in all other markets
+    function updateBinance() {
+        $.ajax({
+            method: "GET",
+
+            url: "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
+            dataType: 'json',
+        }).success(function (data, x, y) {
+            if (!data || !data.price) return
+            var price = parseFloat(data.price).toFixed(2)
+
+            if (price > currentPrice) {
+                price = '<span class="green">' + price + '</span>'
+            } else {
+                price = '<span class="red">' + price + '</span>'
+            }
+            $binanceBox.html(price)
+        });
     }
 
 
-    function addOrdersInfoBoxes(){
-        if($('.sellOrderSizeTotal')[0]) return
+    function addOrdersInfoBoxes() {
+        if ($('.sellOrderSizeTotal')[0]) return
+
+        var $binanceBoxInner = $('<div class="binanceBox"></div>')
+        $('[class*="OrderForm_order-form_"]').after($binanceBoxInner)
+        $binanceBox = $('.binanceBox')
 
         var $sellsOrderSizeInfoBox = $('<div class="sellBtn infoBox"> <div>Size</div> <div class="sellOrderSizeTotal">-</div></div>')
         $('[class*="UserPanel_cancel"]').after($sellsOrderSizeInfoBox)
@@ -54,108 +77,104 @@ setTimeout(function initGES () {
     }
 
 
-    function calculateOpenOrders(buyOrSell){       
-        var priceSum=0
+    function calculateOpenOrders(buyOrSell) {
+        var priceSum = 0
         var priceSumTxt;
-        var orderSizeSum=0
+        var orderSizeSum = 0
         var orderSizeSumTxt;
         addOrdersInfoBoxes()
 
         $('[class*=" OrderList_row"]').each(function (i, el) {
-            if(el.className.includes('header')) return
-            var $el= $(el)
+            if (el.className.includes('header')) return
+            var $el = $(el)
 
-            if(buyOrSell==='sell' && $el.html().includes('OrderList_sell_') ===false) return
-            if(buyOrSell==='buy' && $el.html().includes('OrderList_buy_') ===false) return 
+            if (buyOrSell === 'sell' && $el.html().includes('OrderList_sell_') === false) return
+            if (buyOrSell === 'buy' && $el.html().includes('OrderList_buy_') === false) return
 
-           var orderSizeCol= $el.find($('[class*="OrderList_order-size_"]'))
-           var orderSize=orderSizeCol.text().match(/\d+/g)
-           orderSize = Number.parseFloat(orderSize[0] + '.' +orderSize[1])
-        
-           var priceCol= $el.find($('[class*="OrderList_order-price_"]'))
-           var desiredPrice= combineWholeAndPart($(priceCol))
-           var calculatedSalePrice=(desiredPrice * orderSize)
-           priceSum += calculatedSalePrice
-           orderSizeSum += orderSize
-            
-           //insert the final price after the desired price
-           updateCalculatedPrice(priceCol, calculatedSalePrice)
-        })    
+            var orderSizeCol = $el.find($('[class*="OrderList_order-size_"]'))
+            var orderSize = orderSizeCol.text().match(/\d+/g)
+            orderSize = Number.parseFloat(orderSize[0] + '.' + orderSize[1])
 
-        if(!orderSizeSum) {
-            orderSizeSumTxt='-'
-        }else {
+            var priceCol = $el.find($('[class*="OrderList_order-price_"]'))
+            var desiredPrice = combineWholeAndPart($(priceCol))
+            var calculatedSalePrice = (desiredPrice * orderSize)
+            priceSum += calculatedSalePrice
+            orderSizeSum += orderSize
+
+            //insert the final price after the desired price
+            updateCalculatedPrice(priceCol, calculatedSalePrice)
+        })
+
+        if (!orderSizeSum) {
+            orderSizeSumTxt = '-'
+        } else {
             orderSizeSumTxt = orderSizeSum.toFixed(5).toString()
         }
 
-        if(!priceSum) {
-            priceSumTxt='-'
-        }else {
-            priceSumTxt = currencySymbol+priceSum.formatMoney()
+        if (!priceSum) {
+            priceSumTxt = '-'
+        } else {
+            priceSumTxt = currencySymbol + priceSum.formatMoney()
         }
-         
 
-        if(buyOrSell==='sell'){
+        if (buyOrSell === 'sell') {
             $sellsOrderSizeTotal.text(orderSizeSumTxt)
             $sellsSum.text(priceSumTxt)
-        }else {
+        } else {
             $buyOrderSizeTotal.text(orderSizeSumTxt)
             $buysSum.text(priceSumTxt)
         }
-     
-
     }
 
-    function updateCalculatedPrice(priceCol, calculatedSalePrice){
-        var newHtml='<span class="calculatedPrice">(' + currencySymbol +  calculatedSalePrice.formatMoney() + ')</span>'
+    function updateCalculatedPrice(priceCol, calculatedSalePrice) {
+        var newHtml = '<span class="calculatedPrice">(' + currencySymbol + calculatedSalePrice.formatMoney() + ')</span>'
 
-        if(priceCol.html().includes('calculatedPrice') ==false){
+        if (priceCol.html().includes('calculatedPrice') == false) {
             priceCol.append(newHtml)
-        }else {
-           var found= priceCol.find($('.calculatedPrice'))
-           found.html(newHtml)
+        } else {
+            var found = priceCol.find($('.calculatedPrice'))
+            found.html(newHtml)
         }
-        
+
     }
 
     function combineWholeAndPart($el) {
-        var tmp=$el.text()
-        if($el.text().includes('MKT')) return currentPrice
+        var tmp = $el.text()
+        if ($el.text().includes('MKT')) return currentPrice
 
-        var wholeDiv=$el.find($('.whole'))
-        var whole=wholeDiv.text()
+        var wholeDiv = $el.find($('.whole'))
+        var whole = wholeDiv.text()
 
-        var partDiv=$el.find($('.part'))
-        var part=partDiv.text()
+        var partDiv = $el.find($('.part'))
+        var part = partDiv.text()
         return Number.parseFloat(whole + '.' + part)
     }
 
 
-    function updateCurrentPrice(){
-        var found=$('[class*="ZoomControls_midprice_"]')
-        var tmp=found.text()
-        currentPrice=parseNumber(found.text())
+    function updateCurrentPrice() {
+        var found = $('[class*="ZoomControls_midprice_"]')
+        var tmp = found.text()
+        currentPrice = parseNumber(found.text())
     }
 
-    Number.prototype.formatMoney = function(c, d, t){
-        var n = this, 
-            c = isNaN(c = Math.abs(c)) ? 2 : c, 
-            d = d == undefined ? "." : d, 
-            t = t == undefined ? "," : t, 
-            s = n < 0 ? "-" : "", 
-            i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    Number.prototype.formatMoney = function (c, d, t) {
+        var n = this,
+            c = isNaN(c = Math.abs(c)) ? 2 : c,
+            d = d == undefined ? "." : d,
+            t = t == undefined ? "," : t,
+            s = n < 0 ? "-" : "",
+            i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
             j = (j = i.length) > 3 ? j % 3 : 0;
-           return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-         };
+        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    };
 
-         function parseNumber(str)
-         {
-             str = (str + '').replace(/[^\d,.-]/g, '')    // just digits, separators and sign
-             var sign = str.charAt(0) === '-' ? '-' : '+' // store sign
-             var minor = str.match(/[.,](\d+)$/)          // filter decimals
-             str = str.replace(/[.,]\d*$/, '').replace(/\D/g, '')      // remove decimals and any integer separator
-             return Number(sign + str + (minor ? '.' + minor[1] : '')) // build number
-         }
+    function parseNumber(str) {
+        str = (str + '').replace(/[^\d,.-]/g, '')    // just digits, separators and sign
+        var sign = str.charAt(0) === '-' ? '-' : '+' // store sign
+        var minor = str.match(/[.,](\d+)$/)          // filter decimals
+        str = str.replace(/[.,]\d*$/, '').replace(/\D/g, '')      // remove decimals and any integer separator
+        return Number(sign + str + (minor ? '.' + minor[1] : '')) // build number
+    }
 
 
 
--- ticker.js
+++ ticker.js
@@ -1,42 +1,39 @@
-setTimeout(function initGES () {
-    var $els = []    
-    var root=$('[class*="Navbar_nav-wrapper_"]')
-    var $ticker=$('<marquee class="topTicker"></marquee>')
+setTimeout(function initGES() {
+    var $els = []
+    var root = $('[class*="Navbar_nav-wrapper_"]')
+    var $ticker = $('<marquee class="topTicker"></marquee>')
     root.append($ticker)
-    setInterval(updatePrices,5000)
+    setInterval(updatePrices, 5000)
 
-    function updatePrices(){
+    function updatePrices() {
         $.ajax({
             method: "GET",
             url: "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=20",
-            dataType: 'json'  
-          }).success(function(data,x,y){
-            if(!data || !data[0] ) return
+            dataType: 'json'
+        }).success(function (data, x, y) {
+            if (!data || !data[0]) return
 
-            data.forEach(function(pair) {
-                createOrUpdateSymbolEl(pair)  
-
+            data.forEach(function (pair) {
+                createOrUpdateSymbolEl(pair)
             });
-            
-
         });
     }
 
-    function createOrUpdateSymbolEl(pair){
+    function createOrUpdateSymbolEl(pair) {
 
-        var color='green'
-        if(parseFloat(pair.percent_change_1h) < 0 ){
-            color='red';
+        var color = 'green'
+        if (parseFloat(pair.percent_change_1h) < 0) {
+            color = 'red';
         }
 
-        newHtml=pair.symbol + '<span class="'+color+' topPrice"> '+pair.price_usd+'</span>'
+        newHtml = pair.symbol + '<span class="' + color + ' topPrice"> ' + pair.price_usd + '</span>'
 
 
-        if($els[pair.symbol]) {
-             $els[pair.symbol].html(newHtml);
-        }else {
-            $els[pair.symbol] =$('<span class="topPair">'+pair.symbol+'</span>')
-            $ticker.append($els[pair.symbol])        
+        if ($els[pair.symbol]) {
+            $els[pair.symbol].html(newHtml);
+        } else {
+            $els[pair.symbol] = $('<span class="topPair">' + pair.symbol + '</span>')
+            $ticker.append($els[pair.symbol])
             $els[pair.symbol].html(newHtml);
         }
     }
